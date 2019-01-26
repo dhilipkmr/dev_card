fd2b279a4482b1e6753186bccf6e4af135207425
dhilipkmr <dhilip1211@gmail.com>
2019-01-26 21:03:09 +0530

    Dev Card

---
+++ build.js
@@ -0,0 +1,49 @@
+'use strict'
+
+// Pull in our modules
+const chalk = require('chalk')
+const boxen = require('boxen')
+const fs = require('fs')
+const path = require('path')
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'single'
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white.bold('               DHILIP KUMAR'),
+  work: chalk.hex('#be8d30')('Senior Software Engineer ') +  chalk.white('@ ') + chalk.hex('#f26722')('go') + chalk.hex('#2C67B3')('ibibo'),
+  github: chalk.green('https://github.com/dhilipkmr'),
+  linkedin: chalk.cyan('https://linkedin.com/in/dhilipkmr'),
+  medium: chalk.hex('#adadad')(' https://medium.com/@dhilipkmr'),
+  web: chalk.hex('#cbdc00')(' https://dhilipkmr.surge.sh'),
+  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
+  labelWork: chalk.white.bold('       Work:'),
+  labelGitHub: chalk.white.bold('     GitHub:'),
+  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+  labelMedium: chalk.white.bold('     Medium:'),
+  labelWeb: chalk.white.bold('        Web:')
+}
+
+// Actual strings we're going to output
+const newline = '\n';
+const heading = `${data.name}`;
+const working = `${data.labelWork}  ${data.work}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
+const mediumBlogging = `${data.labelMedium} ${data.medium}`;
+const webing = `${data.labelWeb} ${data.web}`;
+
+const output = heading + // data.name + data.handle
+               newline + newline + // Add one whole blank line
+               working + newline + // data.labelWork + data.work
+               githubing + newline + // data.labelGitHub + data.github
+               linkedining + newline + // data.labelLinkedIn + data.linkedin
+               mediumBlogging + newline + // data.labelLinkedIn + data.linkedin
+               webing;// data.labelWeb + data.web
+
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))

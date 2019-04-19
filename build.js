d23d95e8e1faae4e62c7e9b0cdf1999bab4607cb
dhilipkmr <dhilip1211@gmail.com>
2019-04-19 22:33:03 +0530

    Update

--- build.js
+++ build.js
@@ -19,15 +19,15 @@ const data = {
   work: chalk.hex('#be8d30')('Senior Software Engineer ') +  chalk.white('@ ') + chalk.hex('#f26722')('go') + chalk.hex('#2C67B3')('ibibo'),
   github: chalk.green('https://github.com/dhilipkmr'),
   linkedin: chalk.cyan('https://linkedin.com/in/dhilipkmr'),
-  medium: chalk.hex('#adadad')(' https://medium.com/@dhilipkmr'),
+  blog: chalk.hex('#adadad')(' https://dev.to/dhilipkmr'),
   npm: chalk.red(' https://www.npmjs.com/~dhilipkmr'),
-  web: chalk.hex('#cbdc00')(' https://dhilipkmr.surge.sh'),
-  labelWork: chalk.white.bold('       Work:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelMedium: chalk.white.bold('     Medium:'),
-  labelNpm: chalk.white.bold('        Npm:'),
-  labelWeb: chalk.white.bold('        Web:')
+  web: chalk.hex('#cbdc00')(' https://dhilipkmr.dev'),
+  labelWork:      chalk.white.bold('       Work:'),
+  labelGitHub:    chalk.white.bold('     GitHub:'),
+  labelLinkedIn:  chalk.white.bold('   LinkedIn:'),
+  labelDev:       chalk.white.bold('      Blogs:'),
+  labelNpm:       chalk.white.bold('        Npm:'),
+  labelWeb:       chalk.white.bold('    Website:')
 }
 
 // Actual strings we're going to output
@@ -36,17 +36,18 @@ const heading = `${data.name}`;
 const working = `${data.labelWork}  ${data.work}`;
 const githubing = `${data.labelGitHub}  ${data.github}`;
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
-const mediumBlogging = `${data.labelMedium} ${data.medium}`;
+const devBlogging = `${data.labelDev} ${data.blog}`;
 const npming = `${data.labelNpm} ${data.npm}`;
 const webing = `${data.labelWeb} ${data.web}`;
 
 const output = heading +
                newline + newline +
                working + newline +
-               githubing + newline +
+               webing + newline +
                linkedining + newline +
-               mediumBlogging + newline +
-               npming + newline +
-               webing;
+               githubing + newline +
+               devBlogging + newline +
+               npming + newline;
+               
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
